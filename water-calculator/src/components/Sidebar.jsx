3021dab630ebc6a0475b10ba92a82770d668a7c6
Yordan Markov <i524517>
2024-12-28 19:23:42 +0200

    Made the Sidebar with expandable dots for more than one question in each category. 28.12.2024

--- water-calculator/src/components/Sidebar.jsx
+++ water-calculator/src/components/Sidebar.jsx
@@ -1,22 +1,33 @@
 import './Sidebar.css';
+import React, { useState } from 'react';
 
 function Sidebar() {
+  const [showBathCircles, setShowBathCircles] = useState(false);  
+  const [showSinkCircles, setShowSinkCircles] = useState(false);  
+
   return (
     <div className="sidebar">
-      {/* home.svg -> 1
-      bath.svg -> 5
-      toilet.svg -> 1
-      sink.svg -> 2
-      dish.svg-> 1
-      washingmachine.svg -> 1
-      graywater.svg -> 1 */}
-
       <p className="sidebar-title"> Вътрешна вода </p>
       <div className="items">
         <img className="item" src={require("../images/icons/home.svg").default} alt="home"></img>
-        <img className="item" src={require("../images/icons/bath.svg").default} alt="bath"></img>
+        <div onMouseEnter={() => setShowBathCircles(true)} onMouseLeave={() => setShowBathCircles(false)}>
+            <img className="item" src={require("../images/icons/bath.svg").default} alt="bath"></img>
+            <div className={`circles ${showBathCircles ? 'expanded' : ''}`}>
+                <img className="circle" src={require("../images/icons/circle.svg").default} alt="circle"></img>
+                <img className="circle" src={require("../images/icons/circle.svg").default} alt="circle"></img>
+                <img className="circle" src={require("../images/icons/circle.svg").default} alt="circle"></img>
+                <img className="circle" src={require("../images/icons/circle.svg").default} alt="circle"></img>
+                <img className="circle" src={require("../images/icons/circle.svg").default} alt="circle"></img>
+            </div>
+        </div>
         <img className="item" src={require("../images/icons/toilet.svg").default} alt="toilet"></img>
-        <img className="item" src={require("../images/icons/sink.svg").default} alt="sink"></img>
+        <div onMouseEnter={() => setShowSinkCircles(true)} onMouseLeave={() => setShowSinkCircles(false)}>
+            <img className="item" src={require("../images/icons/sink.svg").default} alt="sink"></img>
+            <div className={`circles ${showSinkCircles ? 'expanded' : ''}`}>
+                <img className="circle" src={require("../images/icons/circle.svg").default} alt="circle"></img>
+                <img className="circle" src={require("../images/icons/circle.svg").default} alt="circle"></img>
+            </div>
+        </div>
         <img className="item" src={require("../images/icons/dish.svg").default} alt="dish"></img>
         <img className="item" src={require("../images/icons/washingmachine.svg").default} alt="washingmachine"></img>
         <img className="item" src={require("../images/icons/graywater.svg").default} alt="graywater"></img>
