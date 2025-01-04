df270916f2849515bc7932879b18aa057c9a1614
Yordan Markov <i524517>
2025-01-04 12:46:06 +0200

    On the question with the Bulgarian map I made the image to change to the current selected area. 04.01.2025

--- water-calculator/src/components/QuestionBG.jsx
+++ water-calculator/src/components/QuestionBG.jsx
@@ -19,9 +19,44 @@ function QuestionBG() {
     "Габрово", "Перник", "Смолян", "Ямбол"
   ];
 
+  const areaImageMap = {
+    "Благоевград": require("../images/all/areas/Blagoevgrad.svg").default,
+    "Добрич": require("../images/all/areas/Dobrich.svg").default,
+    "Плевен": require("../images/all/areas/Pleven.svg").default,
+    "София": require("../images/all/areas/Sofiiska.svg").default,
+    "Бургас": require("../images/all/areas/Burgas.svg").default,
+    "Кърджали": require("../images/all/areas/Kurdzhali.svg").default,
+    "Пловдив": require("../images/all/areas/Plovdiv.svg").default,
+    "София (столица)": require("../images/all/areas/Sofia.svg").default,
+    "Варна": require("../images/all/areas/Varna.svg").default,
+    "Кюстендил": require("../images/all/areas/Kyustendil.svg").default,
+    "Разград": require("../images/all/areas/Razgrad.svg").default,
+    "Стара Загора": require("../images/all/areas/Stara Zagora.svg").default,
+    "Велико Търново": require("../images/all/areas/Veliko Turnovo.svg").default,
+    "Ловеч": require("../images/all/areas/Lovech.svg").default,
+    "Русе": require("../images/all/areas/Ruse.svg").default,
+    "Търговище": require("../images/all/areas/Turnovo.svg").default,
+    "Видин": require("../images/all/areas/Vidin.svg").default,
+    "Монтана": require("../images/all/areas/Montana.svg").default,
+    "Силистра": require("../images/all/areas/Silistra.svg").default,
+    "Хасково": require("../images/all/areas/Haskovo.svg").default,
+    "Враца": require("../images/all/areas/Vratsa.svg").default,
+    "Пазарджик": require("../images/all/areas/Pazardzhik.svg").default,
+    "Сливен": require("../images/all/areas/Sliven.svg").default,
+    "Шумен": require("../images/all/areas/Shumen.svg").default,
+    "Габрово": require("../images/all/areas/Gabrovo.svg").default,
+    "Перник": require("../images/all/areas/Pernik.svg").default,
+    "Смолян": require("../images/all/areas/Smolyan.svg").default,
+    "Ямбол": require("../images/all/areas/Yambol.svg").default,
+  };
+
+  const getImageSource = () => {
+    return areaImageMap[selectedArea] || require("../images/all/bg.png");
+  };
+
   return (
     <div className="bulgaria-container">
-      <img className="bulgaria" src={require("../images/all/bg.png")} alt="Bulgaria" />
+      <img className="bulgaria" src={getImageSource()} alt="Bulgaria" />
 
       <FormControl sx={{ minWidth: 200, marginTop: 2 }}>
         <InputLabel
@@ -34,7 +69,7 @@ function QuestionBG() {
             opacity: selectedArea ? 0 : 1,
             transition: 'opacity 0.3s ease',
           }}
-          shrink={selectedArea ? true : false}
+          shrink={!!selectedArea}
         >
           Изберете област
         </InputLabel>
